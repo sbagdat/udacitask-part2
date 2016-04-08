8f1c45c84523991d5132dd236a739da0c506f54b
Sıtkı Bağdat <sbagdat@gmail.com>
2016-04-09 00:00:31 +0300

    Add Listable#format_date to avoid repetition

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,10 @@ module Listable
   def format_description(description)
     "#{description}".ljust(25)
   end
+
+  def format_date(*dates)
+    empty_message = dates.size == 2 ? 'N/A' : 'No due date'
+    formatted_date = dates.compact.map! { |d| d.strftime("%D") }.join ' -- '
+    formatted_date.empty? ? empty_message : formatted_date
+  end
 end
--- lib/todo.rb
+++ lib/todo.rb
@@ -7,9 +7,7 @@ class TodoItem
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_date
-    @due ? @due.strftime("%D") : "No due date"
-  end
+
   def format_priority
     value = " ⇧" if @priority == "high"
     value = " ⇨" if @priority == "medium"
@@ -17,9 +15,10 @@ class TodoItem
     value = "" if !@priority
     return value
   end
+
   def details
     format_description(@description) + "due: " +
-    format_date +
+    format_date(@due) +
     format_priority
   end
 end
